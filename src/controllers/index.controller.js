2caf53dbc1b2999855000b66eea7fa3e44880110
Largom <lgcworker@gmail.com>
2024-02-20 19:31:58 +0100

    Prueba node psql

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,18 @@
+
+const {Pool} = require('pg');
+
+// const connUrl = postgres://{username}:{passoword}@{endpoint}:{port}/{database} 
+
+const pool = new Pool({
+    host: 'dbteam24proyect.cfkqgu0uqs4g.us-east-1.rds.amazonaws.com',
+    user: 'team2424',
+    password: 'team2424',
+    database: 'DBProduction',
+    port: '5432'
+});
+const getUsers = async (req,res)=>{
+    const response = await pool.query('select * from carowner');
+    res.json(response.rows);
+}
+
+module.exports = {getUsers}
