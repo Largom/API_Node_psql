9f26af83c61e1a006de4e3ef4d5d275624bb2179
Largom <lgcworker@gmail.com>
2024-02-24 17:34:51 +0100

    Unificacion rutas

--- src/index.js
+++ src/index.js
@@ -14,14 +14,16 @@ app.use(require('./routes/index'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
-
 //start http server
 const httpServer = http.createServer(app);
 const port = 18000
 httpServer.listen(port);
 console.log('http server listening at port ${port}');
 
+app.get('/', function(request, response){
+    response.sendFile(__dirname +'/views/index.html');
+ })
+ 
 module.exports = {app};
 
 //app.listen(18000);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = Router();
 
 
 const  { getCars, getCarById, getBucketId } = require('../controllers/index.controller')
-router.get('/getALL', getCars);
+router.get('/get/all', getCars);
 router.get('/get/:id', getCarById);
 router.get('/get/s3/:id', getBucketId)
 
