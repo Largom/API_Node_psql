6e0cbe651f168679529587e6b795cbea793cbf4d
Largom <lgcworker@gmail.com>
2024-02-25 21:32:08 +0100

    upd

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ const router = Router();
 const  { getCars, getCarById, getBucketId, getId2,getId1 } = require('../controllers/index.controller')
 router.get('/get/all', getCars);
 router.get('/get/:id', getCarById);
-router.get('/get/s3', getBucketId);
-router.get('/get/s2', getId2);
-router.get('/get/string', getCars);
+router.get('/get/s3/', getBucketId);
+router.get('/get/s2/', getId2);
+router.get('/get/s1/', getId1);
 
 module.exports = router;
