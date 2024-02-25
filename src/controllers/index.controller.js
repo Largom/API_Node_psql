59b94b67f1dc1df8834d99fe3cd7145d8a73fb11
Largom <lgcworker@gmail.com>
2024-02-25 20:58:22 +0100

    RDS

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -6,15 +6,15 @@ const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 // const connUrl = postgres://{username}:{passoword}@{endpoint}:{port}/{database} 
 
 const pool = new Pool({
-    host: 'dbteam24proyect.cfkqgu0uqs4g.us-east-1.rds.amazonaws.com',
-    user: 'team2424',
-    password: 'team2424',
-    database: 'DBProduction',
-    //port: '5432',
-    //user: 'javi',
-    //host: 'connectedcarinstance.cxpfpf5cvaud.us-east-1.rds.amazonaws.com',
-    //database: 'connectedcardb',
-    //password: 'javijavi',
+    //host: 'dbteam24proyect.cfkqgu0uqs4g.us-east-1.rds.amazonaws.com',
+    //user: 'team2424',
+    //password: 'team2424',
+    //database: 'DBProduction',
+    port: '5432',
+    user: 'javi',
+    host: 'connectedcarinstance.cxpfpf5cvaud.us-east-1.rds.amazonaws.com',
+    database: 'connectedcardb',
+    password: 'javijavi',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
