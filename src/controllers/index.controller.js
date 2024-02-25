0a2345f1671d9eb4b67d9e7d87bb13b171da56ba
Largom <lgcworker@gmail.com>
2024-02-25 21:18:49 +0100

    upd

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -6,16 +6,16 @@ const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 // const connUrl = postgres://{username}:{passoword}@{endpoint}:{port}/{database} 
 
 const pool = new Pool({
-    //host: 'dbteam24proyect.cfkqgu0uqs4g.us-east-1.rds.amazonaws.com',
-    //user: 'team2424',
-    //password: 'team2424',
-    //database: 'DBProduction',
+    host: 'dbteam24proyect.cfkqgu0uqs4g.us-east-1.rds.amazonaws.com',
+    user: 'team2424',
+    password: 'team2424',
+    database: 'DBProduction',
     port: '5432',
-    user: 'javi',
-    host: 'connectedcarinstance.cxpfpf5cvaud.us-east-1.rds.amazonaws.com',
-    database: 'connectedcardb',
-    password: 'javijavi',
-    port: 5432,
+    //user: 'javi',
+    //host: 'connectedcarinstance.cxpfpf5cvaud.us-east-1.rds.amazonaws.com',
+    //database: 'connectedcardb',
+    //password: 'javijavi',
+    //port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
@@ -63,4 +63,4 @@ const getBucketId = async (req,res)=>{
         console.error(err);
       }; 
 }
-module.exports = {getCars, getCarById, getBucketId, getId1,getId2}
+module.exports = {getCars, getCarById, getBucketId, getId1, getId2}
